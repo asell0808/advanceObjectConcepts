209ca95bc9bc0328c248305f2b09af55b5c99e01
Anthony Sellers <asell0808@yahoo.com>
2021-09-13 17:53:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -41,4 +41,33 @@ const object4 = {
 // 3. instantiation
 // makes a copy of an object that reuses the same code as the original
 // ex. multiplayer objects in a video game
+class Player {
+    constructor(name, type){
+        this.name = name;
+        this.type = type;
+        // now every Player will have a name and a type
+    }
+    introduce() {
+        console.log('Hi, I am ' + this.name + ' , I am ' + this.type);
+    }
+}
 
+//this class adds to the Player class
+class Wizard extends Player {
+    constructor(name, type){
+        // whenever you extend a class, super is the syntax to 
+        //pull what you want from the class you are extending
+        super(name, type)
+    }
+    play() {
+        console.log('Weeeee, I am a ' + this.type);
+    }
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Jake', 'Dark Magician');
+
+wizard1.play();
+wizard1.introduce();
+wizard2.play();
+wizard2.introduce();
