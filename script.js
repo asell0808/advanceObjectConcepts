eaca855d88348d651f1e2ad36c4fc4c02f43c512
Anthony Sellers <asell0808@yahoo.com>
2021-09-13 17:40:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,33 @@ object1 == object3;
 
 
 // 2. context
+// context will occaisionally get confused with scope
+// context tells you were you are within the object
+console.log(this);
+// 'this' is the window object.
+this.alert('hello');
 
+window.alert('hello') == this.alert('hello');
+
+function a(){
+    console.log(this);
+}
+
+a();
+// the window object will still be called
+
+window.a();
+
+// to create a new value for 'this'
+const object4 = {
+    a: function(){
+        console.log(this);
+    }
+}
+// this sets 'this' == object4
 
 
 // 3. instantiation
+// makes a copy of an object that reuses the same code as the original
+// ex. multiplayer objects in a video game
 
