0dcddb1adfa72e394e8f5c3fe8d607d023d264a3
Anthony Sellers <asell0808@yahoo.com>
2021-09-13 17:34:21 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// 3 object concepts
+// 1. reference type
+var object1 = {value:10};
+var object2 = object1;
+var object3 = {value: 10};
+
+object1 == object2;
+//true
+
+object1 == object3;
+//false
+
+
+// 2. context
+
+
+
+// 3. instantiation
+
